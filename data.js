ac4ee53dae06190cef7611b546a0a44c1bfb5177
krquinton <nscrexgst.dotr@gmail.com>
2023-05-23 18:05:50 +0800

    initial commit

---
+++ data.js
@@ -0,0 +1,143 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-site-1---a",
+      "name": "Site 1 - A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.34803218213539466,
+        "pitch": 0.4858458958435836,
+        "fov": 1.4385746071217929
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5172502168467581,
+          "pitch": 0.03998096941490914,
+          "rotation": 0,
+          "target": "1-site-1---b"
+        },
+        {
+          "yaw": 1.3952047217594394,
+          "pitch": -0.0021463955044165317,
+          "rotation": 0,
+          "target": "2-site-1---c"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-site-1---b",
+      "name": "Site 1 - B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.6498674169388465,
+        "pitch": 0.5620943679828052,
+        "fov": 1.4385746071217929
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9388401061349763,
+          "pitch": 0.23701809391939577,
+          "rotation": 0,
+          "target": "0-site-1---a"
+        },
+        {
+          "yaw": 1.342601774028381,
+          "pitch": 0.06206650000828873,
+          "rotation": 0,
+          "target": "2-site-1---c"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-site-1---c",
+      "name": "Site 1 - C",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.9324061268955584,
+        "pitch": 0.37269347946436326,
+        "fov": 1.4385746071217929
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.302314712857709,
+          "pitch": 0.3078644457782449,
+          "rotation": 0,
+          "target": "0-site-1---a"
+        },
+        {
+          "yaw": -2.697980129385794,
+          "pitch": 0.03021212641066029,
+          "rotation": 0,
+          "target": "1-site-1---b"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "MCDAI",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
